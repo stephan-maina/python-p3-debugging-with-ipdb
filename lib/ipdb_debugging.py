b91481bc25cb394dc5c0db699f8cfaa7309fe9ba
Mr.Perfect 092 <135037455+stephan-maina@users.noreply.github.com>
2023-09-01 23:37:17 +0300

    Update ipdb_debugging.py

--- lib/ipdb_debugging.py
+++ lib/ipdb_debugging.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python3
-
 import ipdb
 
 def plus_two(num):
-    num + 2
+    num = num + 2  # Add 2 to num and assign it back
     return num
+
+ipdb.set_trace()  # Set a breakpoint
+
+result = plus_two(3)
+
